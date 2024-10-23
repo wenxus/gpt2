04c98d57c92ed95c70fcc8bec609572a759ba399
wenx <wx5rice@gmail.com>
2024-10-22 17:50:05 -0700

    working! may need to polish

--- gpt2numpy.py
+++ gpt2numpy.py
@@ -1,7 +1,6 @@
 #%%
 import torch as t
 from torch import nn, Tensor
-# from torch.distributions.categorical import Categorical
 from torch.nn import functional as F
 from dataclasses import dataclass
 import numpy as np
@@ -13,7 +12,6 @@ import tensorflow as tf
 from tqdm import tqdm
 import math
 from transformers import AutoTokenizer
-# from encoder import get_encoder
 
 @dataclass
 class Config:
@@ -29,11 +27,10 @@ class Config:
     n_layers: int = 12
 
 cfg = Config()
-print(cfg)
 #%%
-def layernorm(x, g, b, eps=1e-5): # x has shape [batch posn d_model]
+def layernorm(x, g, b, eps=1e-5): # x has shape [posn d_model]
     mean = x.mean(-1, keepdims=True)
-    var = x.std(-1,keepdims=True)
+    var = x.var(-1,keepdims=True)
     return (g * (x - mean)/np.sqrt(var + eps)) + b
 
 def embed(wembed, tokens): # "d_vocab, d_model", "position" -> "position d_model"
@@ -45,73 +42,68 @@ def pos_embed(wpembed, tokens): # "n_ctx, d_model", "position" -> "position d_mo
 def unembed(w_u, x):
     return np.einsum('se,ev->sv', x, w_u)
 
-def rand_float_test(input, fn):
-    print("Input shape:", random_input.shape)
-    output = fn(random_input, 1, 0)
-    # if isinstance(layernorm, tuple): output = output[0]
-    print("Output shape:", output.shape, "\n")
-
-#%%
-random_input = np.random.rand(2, 4, 10)
-rand_float_test(random_input, layernorm)
-print("Output shape:", pos_embed(np.random.rand(5,6), np.ones((2,3), dtype=np.integer)).shape)
 # %%
 def apply_casual_mask(attn_scores): # "n_heads query_pos key_pos" -> "n_heads query_pos key_pos"
     mask = 1-np.tri(attn_scores.shape[-1])
-    return np.where(mask, attn_scores, float('-inf'))
+    result = np.where(mask, float('-inf'),attn_scores)
+    return result
 
 def softmax(x):
     return np.exp(x) / np.exp(x).sum()
-    
-# %%
-m = nn.Softmax(dim=1)
-input = t.tensor([[1,2],[3,4]], dtype=t.float32)
-print(m(input))
+#   exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+#   return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 # %%
-def attention(w_q, w_k, w_v, b_q, b_k, b_v, w_o, b_o, x): # x "seq, d_model"
+
+def linear(x, w, b):  # [m, in], [in, out], [out] -> [m, out]
+    return x @ w + b
+
+def attention(Q, K, V, w, b): # x "seq, d_model" -> "seq, d_model"
     # w_k, w_q, w_v, "num_head, d_model, d_head"; w_o "num_head, d_head, d_model"
     # K = w_k@x; Q = w_q@x; A = softmax(apply_casual_mask(QK.T)); 
     # V = w_v@x; z = AV; result = w_o z; atten_out = result.sum(dim=-2)
-    e = x.shape[-1]
-    n, e, h = w_k.shape
-    K = np.einsum('se,enh->snh', x, w_k.reshape(e, n, h)) + b_k # s n h 
-    Q = np.einsum('se,enh->snh', x, w_q.reshape(e, n, h)) + b_q # s n h 
-    attn_scores = np.einsum('snh,hnx->nsx', Q, K.T)/np.sqrt(h) # n s s
-    A = softmax(apply_casual_mask(attn_scores))
-    #TODO
-    V = np.einsum('se,enh->snh', x, w_v.reshape(e, n, h)) + b_v # s n h
-    output = np.einsum('snh,hne->se', V, w_o.reshape(h, n, e)) + b_o
-    print(f"attention {output.shape=}")
+    s,n,h = Q.shape
+    attn_scores = apply_casual_mask(np.einsum('snh,hnx->nsx', Q, K.T)/np.sqrt(h)) # n s s
+    score_list = np.split(attn_scores, n, axis = 0)
+    attn_list = [softmax(s[0]) for s in score_list]
+    A = np.stack(attn_list)
+    z = np.einsum('nsx,xnh->snh', A, V)
+    z = z.reshape(s, n*h)
+    output = np.einsum('sc,ce->se', z, w) + b
     return output
 
 # %%
 def parse_attn_input(x, c_attn, c_proj, n_heads): # w (768x2304), b 2304; c_proj 768x768, 768
-    e, qkvheads = c_attn['w'].shape
-    c_attn_rw = c_attn['w'].reshape(qkvheads, e)
-    d_head = (qkvheads // 3)//n_heads
-    qkv_w = c_attn_rw.reshape(3, n_heads, d_head, e)
-    qkv_w = qkv_w.reshape(3, n_heads, e, d_head)
-    qkv_b = c_attn['b'].reshape(3, n_heads, d_head)
+    s, dmodel = x.shape
+    x = np.einsum('se,ec->sc', x, c_attn['w']) + c_attn['b'] # seq, d_model * 3
 
-    a, e = c_proj['w'].shape
-    w_o = c_proj['w'].reshape(n_heads, a//n_heads, e)
-    print(f"{qkv_w.shape=}")
-    print(f"{qkv_b.shape=}")
-    print(f"{w_o.shape=}")
-    print(f"{x.shape=}")
-
-    return attention(qkv_w[0], qkv_w[1], qkv_w[2], qkv_b[0], qkv_b[1], qkv_b[2], w_o, c_proj['b'], x)
+    qkv = np.split(x, 3, axis=-1)  # [n_seq, 3*n_embd] -> [3, n_seq, n_embd]
+    # split into heads
+    qkv_heads = list(map(lambda x: np.split(x, n_heads, axis=-1), qkv))  # [3, n_seq, n_embd] -> [3, n_head, n_seq, n_embd/n_head]
+    Q = np.array(qkv_heads[0])
+    n,s,h = Q.shape
+    Q = Q.reshape(s,n,h)
+    K = np.array(qkv_heads[1])
+    K = K.reshape(s,n,h)
+    V = np.array(qkv_heads[2])
+    V = V.reshape(s,n,h)
+    return attention(Q, K, V, **c_proj)
 
 def gelu(x):
     tanh_val = np.sqrt(2/math.pi) * (x + 0.044715 * np.power(x, 3))
-    return 0.5 * x * (1 + (np.exp(2*tanh_val) - 1)/(np.exp(2*tanh_val) + 1))
+    return 0.5 * x * (1 + np.tanh(tanh_val))
 
 def mlp_block(x, c_fc, c_proj):
     pre = np.einsum('se,em->sm', x, c_fc['w']) + c_fc['b']
     post = gelu(pre)
     mlp_out = np.einsum('sm,me->se', post, c_proj['w']) + c_proj['b']
     return mlp_out
+
+def layer_norm(x, g, b, eps: float = 1e-5):
+    mean = np.mean(x, axis=-1, keepdims=True)
+    variance = np.var(x, axis=-1, keepdims=True)
+    x = (x - mean) / np.sqrt(variance + eps)  # normalize x to have mean=0 and var=1 over last axis
+    return g * x + b  # scale and offset with gamma/beta params
 
 def transformer_block(x, attn, ln_1, ln_2, mlp, n_heads):
     mid = parse_attn_input(layernorm(x, **ln_1), **attn, n_heads=n_heads) + x
@@ -189,56 +181,8 @@ def load_hparams_and_params(model_size, models_dir):
 
     return hparams, params
 
-# %%
-p = None
-def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", models_dir: str = "models"):
-
-    # load encoder, hparams, and params from the released open-ai gpt-2 files
-    hparams, params = load_hparams_and_params(model_size, models_dir)
-
-    return params, hparams
-    print(f"{params=}")
-    # # encode the input string using the BPE tokenizer
-    # input_ids = encoder.encode(prompt)
-
-    # # make sure we are not surpassing the max sequence length of our model
-    # assert len(input_ids) + n_tokens_to_generate < hparams["n_ctx"]
-
-    # # generate output ids
-    # output_ids = generate(input_ids, params, hparams["n_head"], n_tokens_to_generate)
-
-    # # decode the ids back into a string
-    # output_text = encoder.decode(output_ids)
-
-    # return output_text
-p, hparams = main('today im testing this')
-# %%
-# print((p['blocks'])) # dict_keys(['blocks', 'ln_f', 'wpe', 'wte']) 
-# dict_keys(['attn', 'ln_1', 'ln_2', 'mlp']) # in a list of 12
-
-print(p['blocks'][0]['mlp']['c_fc'].keys()) # dict_keys(['c_attn', 'c_proj'])
-print(p['blocks'][0]['mlp']['c_proj'].keys())
-# print(p['blocks'][0]['attn']['c_attn'].keys()) # dict_keys(['b', 'w'])
-# print(p['blocks'][0]['attn']['c_proj'].keys()) # dict_keys(['b', 'w'])
-
-# # blocks [{'attn': {'c_attn': , 'c_proj'}}, {'ln_1': }, {l_2: }]
-# # print(p['wte'].shape) # (50257, 768)
-# print(len(p['blocks'][0]['attn']['c_proj']['w'])) # 768
-# print(len(p['blocks'][0]['attn']['c_proj']['b'])) # 2304
-# print(p['blocks'][0]['attn']['c_proj']['w'][0].shape) # (768x2304)
-# print(p['blocks'][0]['attn']['c_proj']['b'][0]) # 2304x1
-# %%
-qkv = p['blocks'][0]['attn']['c_attn']['w']
-np.split(qkv, 3, axis=-1)[0].shape
-#%%
-# parsing_attn_input(np.random.rand(2,768), **p['blocks'][0]['attn'], n_heads=hparams["n_head"])
-#%%
-
 def gpt2(tokens, wte, wpe, blocks, ln_f, hparams):
-    print(f"{wte.shape=}")
-    print(f"{wpe.shape=}")
     x = embed(wte, tokens) + pos_embed(wpe, tokens)
-    print(f"{x.shape=}")
     for b in blocks:
         x = transformer_block(x, **b, n_heads=hparams["n_head"])
 
@@ -250,16 +194,14 @@ def run_gpt2(input):
     hparams, params = load_hparams_and_params("124M", "models")
     
     tokens = tokenizer(input).input_ids
-    print(f"{tokens=}")
     result_t = []
-    for i in tqdm(range(20)):
+    for i in tqdm(range(30)):
         logits = gpt2(tokens, **params, hparams=hparams)
         next_token_id = logits[-1,:].argmax(-1)
-        print(f"{next_token_id=}")
         result_t.append(next_token_id)
+        tokens.append(next_token_id)
     
-    return tokenizer.batch_decode(result_t)
-
+    return ''.join(tokenizer.batch_decode(result_t))
 
 import json
 def params_to_json():
@@ -270,5 +212,5 @@ def params_to_json():
         json.dump(p, fp, default=ndarray_to_list)
 
 # %%
-print(run_gpt2('i am testing'))
+print(run_gpt2('A bluffy blue animal named Daisy'))
 # %%
